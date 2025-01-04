d76692a4079d6d787ebb7536d42face50b2f06e6
beagleman777 <bgawel@student.agh.edu.pl>
2025-01-04 20:44:55 +0100

    Edycja danych pracownika (przykładowa).

--- FrontEnd/src/main/java/backend/HR.java
+++ FrontEnd/src/main/java/backend/HR.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Date;
 package backend;
 
 public class HR extends Boss{
@@ -11,8 +13,25 @@ public class HR extends Boss{
         System.out.println("Usunięto pracownika z bazy danych");
     }
 
-    public void employeeUpdate(Employee e, Edit ed){
+    public void employeeUpdate(Employee e, String edit_type){
+        ed = ed.toUpperCase()
+        switch(ed)
+        case "NAZWISKO":
+            System.out.print("Zmień nazwisko na: ");
+            String surname = "Gaweł";
+            Employee.setSurname(surname);
+            Edit ed = new Edit(new Date(), Integer.toString(rand.nextInt(1000000000000)), "Zmiana nazwy pracownika");
+        case "WYPLATA":
+            System.Out.print("Zmień wypłatę na: ");
+            Float salary = 5000;
+            Employee.setSalary(salary);
+            Edit ed = new Edit(new Date(), Integer.toString(rand.nextInt(1000000000000)), "Zmiana wypłaty pracownika");
 
+        case "DNI WOLNE":
+            System.Out.print("Ile dni wolnych: ");
+            Integer days_off = 4;
+            Employee.setDaysOff(days_off);
+            Edit ed = new Edit(new Date(), Integer.toString(rand.nextInt(1000000000000)), "Zmiana liczby dni wolnych pracownika");
     }
 
 }
